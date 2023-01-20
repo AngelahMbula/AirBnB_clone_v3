e438d6b21def9781277a6e6b6908cd069ed74aed
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2023-01-20 18:46:03 +0300

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -5,17 +5,17 @@ Contains the TestDBStorageDocs and TestDBStorage classes
 
 from datetime import datetime
 import inspect
+import os
 import models
-from models import storage
+from models.engine import db_storage
 from models.amenity import Amenity
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
 import json
-import os
 import pep8
 import unittest
 DBStorage = db_storage.DBStorage
@@ -68,21 +68,111 @@ test_db_storage.py'])
                             "{:s} method needs a docstring".format(func[0]))
 
 
+class Db_Storage(unittest.TestCase):
+    """test get a specific obj"""
+
+    def setup():
+        """define environmental variables"""
+        os.environ['HBNB_ENV'] = "test"
+        os.environ['HBNB_MYSQL_USER'] = "hbnb_test"
+        os.environ['HBNB_TYPE_STORAGE'] = "db"
+        os.environ['HBNB_MYSQL_PWD'] = "hbnb_test_pwd"
+        os.environ['HBNB_MYSQL_HOST'] = "localhost"
+        os.environ['HBNB_MYSQL_DB'] = "hbnb_test_db"
+
+    def teardown():
+        """define environmental variables"""
+        os.environ['HBNB_ENV'] = ""
+        os.environ['HBNB_MYSQL_USER'] = ""
+        os.environ['HBNB_TYPE_STORAGE'] = ""
+        os.environ['HBNB_MYSQL_PWD'] = ""
+        os.environ['HBNB_MYSQL_HOST'] = ""
+        os.environ['HBNB_MYSQL_DB'] = ""
+
+    @unittest.skipIf(models.storage_t != 'db', "testing db storage")
+    def test_get_obj_none(self):
+        """check if obj is none"""
+        self.assertEqual(None, models.storage.get("Tupac", 44))
+
+    @unittest.skipIf(models.storage_t != 'db', "testing db storage")
+    def test_no_args(self):
+        """check if are no args passed to get"""
+        with self.assertRaises(TypeError):
+            models.storage.get()
+
+    @unittest.skipIf(models.storage_t != 'db', "testing db storage")
+    def test_one_args(self):
+        """check if one arg passed to get"""
+        with self.assertRaises(TypeError):
+            models.storage.get("State")
+        with self.assertRaises(TypeError):
+            models.storage.get(None)
+
+    @unittest.skipIf(models.storage_t != 'db', "testing db storage")
+    def test_id_one_args(self):
+        """pass one arg into get"""
+        example_state = State(name="splig")
+        example_state.save()
+        e_id = example_state.id
+        with self.assertRaises(TypeError):
+            models.storage.get(id=e_id)
+        with self.assertRaises(TypeError):
+            models.storage.get(id=666)
+
+    @unittest.skipIf(models.storage_t != 'db', "testing db storage")
+    def test_id_one_args_invalid(self):
+        """pass one valid one invalid"""
+        example_state = State(name="splig")
+        example_state.save()
+        e_id = example_state.id
+        self.assertEqual(None, models.storage.get("ragnar_danneskjöld", e_id))
+        self.assertEqual(None, models.storage.get("State", "666"))
+
+    @unittest.skipIf(models.storage_t != 'db', "testing db storage")
+    def test_get_obj(self):
+        """pass in existing obj"""
+        example_state = State(name="rip")
+        models.storage.new(example_state)
+        models.storage.save()
+        id = example_state.id
+        self.assertEqual(id, models.storage.get("State", id).id)
+
+    @unittest.skipIf(models.storage_t != 'db', "testing db storage")
+    def test_get_count(self):
+        """count existing obj"""
+
+        Base.metadata.drop_all(models.storage._DBStorage__engine)
+        Base.metadata.create_all(models.storage._DBStorage__engine)
+        models.storage.close()
+
+        example_state1 = State(name="notorious")
+        models.storage.new(example_state1)
+        example_state2 = State(name="50_cent")
+        models.storage.new(example_state2)
+        id = example_state2.id
+        example_city1 = City(state_id=id, name="zurich")
+        models.storage.new(example_city1)
+        models.storage.save()
+        self.assertEqual(3, models.storage.count())
+        self.assertEqual(0, models.storage.count("Schmoop"))
+        self.assertEqual(2, models.storage.count("State"))
+
+
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
         self.assertIs(type(models.storage.all()), dict)
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_new(self):
         """test that new adds an object to the database"""
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
