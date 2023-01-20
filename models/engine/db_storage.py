f2856cad1183a9bfbaf3b3efabec0653a123e2b2
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2023-01-20 18:40:01 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,31 +84,23 @@ class DBStorage:
         self.__session.close()
 
     def get(self, cls, id):
-        """
-        Returns the object based on the class name and its ID, or
-        None if not found
-        """
-        if cls not in classes.values():
-            return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
-
-        return None
+        """A method to retrieve one object"""
+        obj = None
+        for clss in classes:
+            #  clss is just keys
+            #  could be str or class keyword
+            if cls is classes[clss] or cls is clss:
+                obj = self.__session.query(classes[clss]).\
+                      filter(classes[clss].id == id).first()
+        return obj
 
     def count(self, cls=None):
-        """
-        count the number of objects in storage
-        """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
-
-        return count
+        """count number of obj's in given class"""
+        obj_count = 0
+        for clss in classes:
+            if cls is classes[clss] or cls is clss:
+                obj_count = self.__session.query(classes[clss]).count()
+        if cls is None:
+            for clss in classes.values():
+                obj_count += self.__session.query(clss).count()
+        return obj_count
